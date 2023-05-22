dc4d1e4238399c408d5422087c1e45f536d07f86
Fabio Ciao <fabio.ciao@gmail.com>
2023-05-22 17:04:00 +0200

    prima rotta verso la home page

--- server.py
+++ server.py
@@ -1,3 +1,10 @@
 from flask import Flask
 
 app = Flask(__name__)
+
+@app.route('/', methods=["GET"])
+def home():
+    return {"mesg:":"beneventuo"}
+
+if __name__ == '__main__':
+    app.run(debug=True, port=8000, host='0.0.0.0')
