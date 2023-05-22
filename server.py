d36c7690166ad852eedb89903bebf2f8046496fc
Fabio Ciao <fabio.ciao@gmail.com>
2023-05-22 17:07:00 +0200

    aggiunto comando per avviare il server

--- server.py
+++ server.py
@@ -8,3 +8,9 @@ def home():
 
 if __name__ == '__main__':
     app.run(debug=True, port=8000, host='0.0.0.0')
+    
+# per eseguire lanciare il comando
+# python server.py
+# oppure
+# export FLASK_APP=server.py
+# lask run --port=8000 --host=0.0.0.0
